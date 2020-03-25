abd880d280aba6bcc5de67ba635d3f0c7bdb465e
gitmapd <marcodomingues@gmail.com>
2020-03-25 15:30:59 +0000

    More learns

--- Ex1.py
+++ Ex1.py
@@ -4,5 +4,19 @@ from datetime import date, datetime
 print("Hello World")
 today=datetime.today()
 current=lambda d: datetime.strftime(d,"%d-%m-%Y")
-print(current(today))
 
+print(f'Date is  {current(today)}')
+
+sieve=[True]*10
+newsieve=[]
+dict={}
+for i in range(len(sieve)//2):
+    if sieve[i]:
+        newsieve.append((sieve[i]*i+2))
+    dict[i]=newsieve[i]
+
+for i,v in dict.items():
+    print(f'key={i} and value={v}')
+
+
+print(newsieve)
